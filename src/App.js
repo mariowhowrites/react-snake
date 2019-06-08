97f50053f893ab5e2e5ed17f0e46b9df4f63cf88
Mario Vega <mario@ark.io>
2019-06-08 02:35:59 -0700

    initial react-snake commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,102 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import useSnakeMovement from "./useSnakeMovement";
 
 function App() {
+  const headPosition = useSnakeMovement({
+    width: 6,
+    depth: 6
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        margin: "0 25vw",
+        display: "flex",
+        flexDirection: "column"
+      }}
+    >
+      <BoardRows headPosition={headPosition} />
     </div>
   );
 }
 
 export default App;
+
+function BoardRows({ headPosition }) {
+  const rowsCount = 11;
+  let rows = [];
+  let i = 1;
+
+  for (i; i <= rowsCount; i++) {
+    rows.push({
+      key: i,
+      rowIndex: i,
+      rowsCount,
+      headPosition
+    });
+  }
+
+  return (
+    <React.Fragment>
+      {rows.map(options => (
+        <BoardRow {...options} />
+      ))}
+    </React.Fragment>
+  );
+}
+
+function BoardRow({ rowIndex, rowsCount, headPosition }) {
+  const row = [];
+  const columnsCount = 11;
+  let i = 1;
+
+  for (i; i <= columnsCount; i++) {
+    const options = {
+      rowIndex,
+      columnIndex: i,
+      key: i
+    };
+
+    if (rowIndex === 1 || rowIndex === rowsCount) {
+      options.squareColor = "black";
+    } else {
+      options.squareColor = i === 1 || i === columnsCount ? "black" : "white";
+    }
+
+    if (
+      options.rowIndex === headPosition.depth &&
+      options.columnIndex === headPosition.width
+    ) {
+      options.squareColor = "green";
+    }
+
+    row.push(options);
+  }
+
+  return (
+    <div
+      style={{
+        display: "flex"
+      }}
+    >
+      {row.map(options => (
+        <BoardSquare {...options} />
+      ))}
+    </div>
+  );
+}
+
+function BoardSquare({ squareColor, columnIndex, rowIndex }) {
+  const styles = {
+    width: "60px",
+    height: "60px",
+    backgroundColor: squareColor
+  };
+
+  if (squareColor === "white" || "green") {
+    styles.border = "1px solid grey";
+    styles.boxSizing = "border-box";
+  }
+
+  return <div style={styles} id={`${columnIndex}x${rowIndex}`} />;
+}
---
+++ src/useSnakeMovement.js
@@ -0,0 +1,108 @@
+import { useState, useEffect } from "react";
+
+function useSnakeMovement(startingPosition) {
+  let [headPosition, setHeadPosition] = useState(startingPosition);
+  let [direction, setDirection] = useState("up");
+
+  let moveUp = () => {
+    let newDepth = headPosition.depth - 1;
+
+    if (newDepth < 2) {
+      newDepth = 10;
+    }
+
+    setHeadPosition({
+      width: headPosition.width,
+      depth: newDepth
+    });
+  };
+
+  let moveDown = () => {
+    let newDepth = headPosition.depth + 1;
+
+    if (newDepth > 10) {
+      newDepth = 2;
+    }
+
+    setHeadPosition({
+      width: headPosition.width,
+      depth: newDepth
+    });
+  };
+
+  let moveLeft = () => {
+    let newWidth = headPosition.width - 1;
+
+    if (newWidth < 2) {
+      newWidth = 10;
+    }
+
+    setHeadPosition({
+      depth: headPosition.depth,
+      width: newWidth
+    });
+  };
+
+  let moveRight = () => {
+    let newWidth = headPosition.width + 1;
+
+    if (newWidth > 10) {
+      newWidth = 2;
+    }
+
+    setHeadPosition({
+      depth: headPosition.depth,
+      width: newWidth
+    });
+  };
+
+  useEffect(() => {
+    const keyboardActions = {
+      up: moveUp,
+      down: moveDown,
+      left: moveLeft,
+      right: moveRight
+    };
+
+    const id = setInterval(keyboardActions[direction], 100);
+
+    return () => {
+      clearInterval(id);
+    };
+  });
+
+  useEffect(() => {
+    const handleKeyboardEvents = e => {
+      const keyboardActions = {
+        ArrowUp: "up",
+        ArrowDown: "down",
+        ArrowLeft: "left",
+        ArrowRight: "right"
+      };
+
+      if (Object.keys(keyboardActions).includes(e.key)) {
+        setDirection(keyboardActions[e.key]);
+      }
+    };
+
+    window.addEventListener("keyup", handleKeyboardEvents);
+
+    return () => {
+      window.removeEventListener("keyup", handleKeyboardEvents);
+    };
+  }, [headPosition.width, headPosition.depth]);
+
+  return headPosition;
+}
+
+export default useSnakeMovement;
+
+// what do I need to do?
+
+// one effect for momentum:
+// use direction
+// set interval to move in direction
+// clear interval when direction changes
+
+// one effect for keyboard presses
+// change direction on keyboard press
